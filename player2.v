c724c44d01a25aa2cef39c080dc4f946861ed15b
Mahsa <manmahna26@gamil.com>
2024-01-25 18:50:45 +0330

    player2

--- player2.v
+++ player2.v
@@ -1,5 +1,5 @@
-module firstPlayer(clk, action1, action2, state2, health);
-    input[2:0] action1, action2, state2;
+module secondPlayer(clk, action2, action1, state1, health);
+    input[2:0] action1, action2, state1;
     input clk;
     output[1:0] health;
     reg[1:0] health = 2'b11;
@@ -14,19 +14,19 @@ module firstPlayer(clk, action1, action2, state2, health);
               left2 = 3'b101,
               right1 = 3'b110,
               right2 = 3'b111;
-    reg [2:0] state = player1S0;
+    reg [2:0] state = player2S0;
     always @ (posedge clk)
         case(state)
-            //100
-            player1S0: begin
-                //plyer1 goes to 010
-                if (action1 == right1 || action1 == right2)
-                    assign state = player1S1;
+            //001
+            player2S0: begin
+                //plyer2 goes to 010
+                if (action2 == left1 || action2 == left2)
+                    assign state = player2S1;
                     //health--
-                    if(action2 == kick && state2 == player2S2)
+                    if(action1 == kick && state1 == player1S2)
                         health = health - 2'b01;
                 //waiting count
-                if (action1 == await) begin
+                if (action2 == await) begin
                     wait_count = wait_count + 2'b01;
                     if (wait_count == 2'b10 && health != 2'b11) begin
                         health = health + 2'b01;
@@ -36,26 +36,26 @@ module firstPlayer(clk, action1, action2, state2, health);
                 //else nothing to do, state doesn't change
             end
             //010
-            player1S1: begin
-                //player1 goes to 001
-                if (action1 == right1 || action1 == right2) begin
-                    assign state = player1S2;
+            player2S1: begin
+                //player2 goes to 100
+                if (action2 == left1 || action2 == left2) begin
+                    assign state = player2S2;
                     //health -= 1
-                    if ((action2 == kick) && (state2 == player2S1)) 
+                    if ((action1 == kick) && (state1 == player1S1)) 
                         health = health - 2'b01;
                     //health -= 2
-                    else if ((action2 == punch) && (state2 == player2S2)) 
+                    else if ((action1 == punch) && (state1 == player1S2)) 
                         health = health - 2'b10;
                 end
-                //player1 goes to 100
-                else if ((action1 == left1 || action1 == left2) || 
-                        (action1 == kick && action2 == kick && state2 == player2S2))
-                    assign state = player1S0;
+                //player2 goes to 001
+                else if ((action2 == right1 || action2 == right2) || 
+                        (action1 == kick && action2 == kick && state1 == player1S2))
+                    assign state = player2S0;
                 //state doesn't change, health -= 1
-                else if((action1 == punch || action1 == await) && (action2 == kick) && (state2 == player2S2))
+                else if((action2 == punch || action2 == await) && (action1 == kick) && (state1 == player1S2))
                     health = health - 2'b01;
                 //waiting count
-                if (action1 == await) begin
+                if (action2 == await) begin
                     wait_count = wait_count + 2'b01;
                     if (wait_count == 2'b10 && health != 2'b11) begin
                         health = health + 2'b01;
@@ -64,30 +64,30 @@ module firstPlayer(clk, action1, action2, state2, health);
                 end
                 //else nothing to do, state doesn't change
             end
-            //001
-            player1S2: begin
-                //player1 goes to 010
-                if ((action1 == left1 || action1 == left2) ||
-                    (action1 == punch && action2 == punch && state2 == player2S2)||
-                    (action1 == kick && action2 == kick && state2 != player2S0))
-                        assign state = player1S1;
+            //100
+            player2S2: begin
+                //player2 goes to 010
+                if ((action2 == right1 || action2 == right2) ||
+                    (action1 == punch && action2 == punch && state1 == player1S2)||
+                    (action1 == kick && action2 == kick && state1 != player1S0))
+                        assign state = player2S1;
                         //health--
-                        if((action1 == left1 || action1 == left2) && action2 == kick && state2 == player2S2)
+                        if((action2 == right1 || action2 == right2) && action1 == kick && state1 == player1S2)
                             health = health - 2'b01;
                 //health--, state doesn't change
-                else if(((action1 == await || action1 == right1 || action1 == right2 || action1 == punch)&&
-                        action2 == kick && state2 == player2S1) ||
-                        ((action1 == await || action1 == right1 || action1 == right2)&&
-                    action2 == kick && state2 == player2S2))
+                else if(((action2 == await || action2 == left1 || action2 == left2 || action2 == punch)&&
+                        action1 == kick && state1 == player1S1) ||
+                        ((action2 == await || action2 == left1 || action2 == left2)&&
+                    action1 == kick && state1 == player1S2))
                         health = health - 2'b01;
                 //health -= 2, state doesn't change
-                else if(((action1 == await || action1 == right1 || action1 == right2 || action1 == kick)&&
-                    action2 == punch && state2 == player2S2))
+                else if(((action2 == await || action2 == left1 || action2 == left2 || action2 == kick)&&
+                    action1 == punch && state1 == player1S2))
                         health = health - 2'b10;
                         
                
                 //waiting count
-                if (action1 == await) begin
+                if (action2 == await) begin
                     wait_count = wait_count + 2'b01;
                     if (wait_count == 2'b10 && health != 2'b11) begin
                         health = health + 2'b01;
