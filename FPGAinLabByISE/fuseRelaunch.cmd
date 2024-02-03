0256a8a8e9b3d8772ed726f56f1547ff45c7edc8
Mahsa <manmahna26@gmail.com>
2024-02-03 21:24:10 +0330

    a few changes

--- FPGAinLabByISE/fuseRelaunch.cmd
+++
@@ -1,1 +0,0 @@
--intstyle "ise" -incremental -lib "unisims_ver" -lib "unimacro_ver" -lib "xilinxcorelib_ver" -o "D:/proj/logicproj/testfpga_isim_beh.exe" -prj "D:/proj/logicproj/testfpga_beh.prj" "work.testfpga" "work.glbl" 
